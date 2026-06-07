73fc0c3a1eadbb6dff4be57a4c340bb0ca18b9ee
Darwish Fil <12131203@Students.liu.edu.lb>
2026-06-07 20:26:04 +0300

    Fix Render Docker startup

--- backend/docker/entrypoint.sh
+++ backend/docker/entrypoint.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
 
+php artisan config:clear
+php artisan cache:clear
+php artisan route:clear
+
 php artisan migrate --force
 php artisan storage:link || true
+
 php artisan config:cache
 php artisan route:cache
 
-/usr/bin/supervisord -c /etc/supervisord.conf
+php artisan serve --host=0.0.0.0 --port=${PORT:-10000}
